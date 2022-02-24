d20062a323bdf5d3ee53befccc89aef6bfdbdf2e
slu2023 <slu2023@chadwickschool.org>
2022-02-24 12:54:19 -0800

    Working on method stubs

--- Program.cs
+++ Program.cs
@@ -1,12 +1,93 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace slu_personal_project_proposal_no2
 {
     class Program
     {
+        public static object CipherTranslation;
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
         }
+
+        // <summary>
+        // Ask the user to type an input, store it for future use.
+        // </summary>
+        // <param name="prompt">The message to display to the user</param>
+        // <returns>The positive number the user chose</returns>
+        public static string Pronoun ()
+        {
+            // 1. Display the question
+            // 2. Take the user input
+        }
+
+        // <summary>
+        // Given a series of choices to the user
+        // Looping until the user picked a valide choice.
+        // </summary>
+        // <param name="prompt">The message to display to the user</param>
+        // <returns>The positive number the user chose</returns>
+        
+        public static int Choice (List<string> Choice)
+        {
+            // 1. Display the question with the user input
+            // 2. Take the user input
+            // 3. Bring the user to one of the rooms
+            // 4. Display invalided input if the userinput is out of range
+        }
+
+        // <summary>
+        // Asking the user to type some valide input.
+        // Looping until the user typed a valide code.
+        // </summary>
+        // <param name="prompt">The message to display to the user</param>
+        // <returns>The positive number the user chose</returns>
+        public static string ENG_CS (string text)
+        {
+            static string UserInput()
+            {
+                // 1. Display "What do you want to generate?"
+                // 2. Waiting for the user to type something
+                // 3. Run test
+                // 4. Display result
+            }
+            return result;
+
+        }
+
+        // <summary>
+        // Asking the user to type some valide input.
+        // Looping until the user typed a valide code.
+        // </summary>
+        // <param name="prompt">The message to display to the user</param>
+        // <returns>The positive number the user chose</returns>
+        public static string CS_ENG (string text)
+        {
+            static string UserInput()
+            {
+                // 1. Display "What do you want to generate?"
+                // 2. Waiting for the user to type something
+                // 3. Run test
+                // 4. Display result
+            }
+            return result;
+
+        }
+
+        // <summary>
+        // Given a questions to the user, the user needs to answer it.  
+        // Looping until the question has been answered.
+        // </summary>
+        // <param name="prompt">The message to display to the user</param>
+        // <returns>The positive number the user chose</returns>
+        public static int Choice (List<string> Decision)
+        {
+            // 1. Display "Do you want to generate another one?"
+            // 2. If the user selected yes, loop back all the way to the beginning.
+            // 3. If the user picked no, display goodbye and end of program.
+            // 4. Else, invalided pick
+        }
+    
     }
 }
